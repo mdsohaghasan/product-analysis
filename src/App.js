25afb6a50bbab985d29e6f1b2a39d257e9b08e70
Sohag Hasan <mailsohaghasan@gmail.com>
2022-04-05 13:25:38 +0600

    route added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Home from './Components/Home/Home';
-// import Reviews from './Components/Reviews/Reviews'
-// import Dashboard from './Components/Dashboard/Dashboard';
-// import About from './Components/About/About';
-// import Blogs from './Components/Blogs/Blogs';
-// import NotFound from './Components/NotFound/NotFound';
-// import { Routes, Route, Link } from "react-router-dom";
+import Reviews from './Components/Reviews/Reviews'
+import Dashboard from './Components/Dashboard/Dashboard';
+import About from './Components/About/About';
+import Blogs from './Components/Blogs/Blogs';
+import NotFound from './Components/NotFound/NotFound';
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
 
 
@@ -15,7 +15,17 @@ function App() {
   return (
     <div className="App">
 
-      <Home></Home>
+      {/* <Home></Home> */}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="Reviews" element={<Reviews />} />
+        <Route path="Dashboard" element={<Dashboard />} />
+        <Route path="about" element={<About />} />
+        <Route path="Blogs" element={<Blogs />} />
+        <Route path="NotFound" element={<NotFound />} />
+      </Routes>
 
       {/* <Router>
         <Link to="/Home">Home</Link>
